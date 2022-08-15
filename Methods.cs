4671d2d0df37445808c14837a8c449ab000432f3
Evan Mckenzie <615evan@gmail.com>
2022-08-15 08:53:39 -0400

    Initial Commit 8/13 HW

---
+++ Methods.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Methods
+    {
+        public static int OneThousand()
+        {
+            {
+                var List1000 = new List<int>();
+                int start = 1000;
+
+                do
+                {
+                    Console.WriteLine($" {start}");
+                    start--;
+                    List1000.Add(start);
+
+                } while (start >= -1000);
+                return start;   
+
+            }
+
+
+
+        }
+        public static int MultiplesOf3()
+        {
+            var ListOfThrees = new List<int>();
+
+            for (int threeXthree = 3; threeXthree <= 999 ; threeXthree += 3) 
+            {
+                ListOfThrees.Add(threeXthree);
+                Console.WriteLine($"{threeXthree}");
+                
+            }
+            return ListOfThrees.Count;
+            
+            
+        }
+
+
+        public static Boolean ToF(int x, int y)
+        {
+             
+            if(x == y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+            
+
+        }
+        
+        public static bool IsItEven(int c)
+        {
+            if (c % 2 == 0)
+            {
+                Console.WriteLine($"{c} is an even number");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{c} is an odd number");
+                return false;
+            }
+
+
+        }
+        
+        public static bool IsItPositive(int num2)
+        {
+            if (num2 < 0)
+            {
+                Console.WriteLine($"{num2} is negative");
+                return false;
+            }
+            else 
+            {
+                Console.WriteLine($"{num2} is positive");
+                return true;
+            }      
+            
+      
+        }
+
+        public static bool AreTheyLegal(int num3)
+        {
+            if (num3 >= 18)
+            {
+                Console.WriteLine($"Congradulations! \n" +
+                    $"You are old enough to vote! \n" +
+                    $"Dont waste it!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You are not old enought to vote! \n" +
+                    "GO HOME");
+                return false;
+            }
+
+
+        }
+
+        public static bool InRange(int num4)
+        {
+            if (num4 >= -10 && num4 <= 10)
+            {
+                Console.WriteLine($"The number {num4} \n" +
+                    $" is in the expected range :)");
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine($"The number {num4} \n" +
+                    $"is out of the expected range, sorry!");
+                return false;
+            }
+        }        
+
+        public static int MultiplicationTable(int num5)
+        {
+            for (int i = 0; i <= 12; i++)
+            {
+                Console.WriteLine($"{num5} x {i} = {num5 * i}");
+            }
+            return num5;
+
+
+        }
+
+
+
+    }
+
+}   
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+
+//Begain Exercise 3
+
+var p1 = Methods.OneThousand();
+Console.WriteLine(p1);
+
+var p2 = Methods.MultiplesOf3();
+Console.WriteLine(p2);
+
+Console.WriteLine("Enter two nmbers to check if theyre equal");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+var p3 = Methods.ToF(a, b);
+
+if (p3 == true)
+{
+    Console.WriteLine($"{a} is equal to {b}");
+}
+else
+{
+    Console.WriteLine($"{a} is not equal to {b}");
+}
+
+Console.WriteLine("Enter a Number to check if it is odd or even:");
+
+var num1 = int.Parse(Console.ReadLine());
+var p4 = Methods.IsItEven(num1);
+
+Console.WriteLine("Enter a number to check if it is positive or negitive");
+
+int num2 = int.Parse(Console.ReadLine());
+var p5 = Methods.IsItPositive(num2);
+
+Console.WriteLine("Please enter your age to see if you're \n" +
+    "old enough to vote");
+
+
+int num3 = int.Parse(Console.ReadLine());
+var p6 = Methods.AreTheyLegal(num3);
+
+Console.WriteLine("please enter a number between -10 and 10 \n" +
+    "to check if it is range... of -10 and 10");
+
+int num4 = int.Parse(Console.ReadLine());
+var p7 = Methods.InRange(num4);
+
+Console.WriteLine("Enter a number to see its multiplication table from 0 - 12");
+
+int num5 = int.Parse(Console.ReadLine());
+var p8 = Methods.MultiplicationTable(num5);
+
+
+
+
+
+
+
+
+
+
